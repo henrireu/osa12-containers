043df54aea9cc0b2ffa9b8cfc5734fdf1769b30c
Henri Reunanen <henkka.reunanen8@gmail.com>
2025-01-30 10:56:06 +0200

    päivitys

--- todo-app/todo-frontend/src/App.jsx
+++ todo-app/todo-frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css';
 import TodoView from './Todos/TodoView'
 
-
 function App() {
+  console.log(import.meta.env.VITE_BACKEND_URL)
   return (
     <div className="App">
       <TodoView />
